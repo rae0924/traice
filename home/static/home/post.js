b23752481b2a5670d557bc1f5c95c86341727fe0
unknown <rae0924384@gmail.com>
2019-09-16 15:59:15 -0500

    submit post fixed

--- home/static/home/post.js
+++ home/static/home/post.js
@@ -1,4 +1,4 @@
-function post() { 
+function detect() { 
     const canvas = document.querySelector(".canvas");
     // const context = canvas.getContext("2d"); 
     const form = document.querySelector(".post-form");
@@ -8,6 +8,23 @@ function post() {
     hiddenField.type = 'hidden';
     hiddenField.name = 'data';
     hiddenField.value = canvas.toDataURL();
+    form.appendChild(hiddenField);
+    document.body.appendChild(form);
+    form.submit();
+}
+function submit_sample(){
+    console.log("hello");
+    const canvas = document.querySelector(".canvas");
+    const form = document.querySelector(".post-form");
+    form.method = 'post';
+    form.action = document.URL;
+    const hiddenField = document.createElement('input');
+    hiddenField.type = 'hidden';
+    hiddenField.name = 'data';
+    var data = {"x": 10};
+    //hiddenField.value = canvas.toDataURL();
+    data = JSON.stringify(data);
+    hiddenField.value = data;
     form.appendChild(hiddenField);
     document.body.appendChild(form);
     form.submit();
