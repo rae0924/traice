bdd7ae7559c2648e801609ce383e9993662c0afb
unknown <rae0924384@gmail.com>
2019-09-20 15:33:30 -0500

    fixed navbar, added about

--- home/urls.py
+++ home/urls.py
@@ -6,5 +6,6 @@ from submission.views import SubmissionView
 urlpatterns = [
     path('', views.home, name='home'),
     path('detector/', DetectorView.as_view(), name='detector'),
-    path('submission/', SubmissionView.as_view(), name='submission')
+    path('submission/', SubmissionView.as_view(), name='submission'),
+    path('about/', views.about, name='about')
 ]
